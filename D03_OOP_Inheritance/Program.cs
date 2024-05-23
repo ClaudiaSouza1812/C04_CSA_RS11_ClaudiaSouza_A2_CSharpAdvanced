871a4f6068ba63a2451cd5fc7f3efa0d7c5d922c
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-05-23 20:10:58 +0100

    240523

--- D03_OOP_Inheritance/Program.cs
+++ D03_OOP_Inheritance/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D00_Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace D03_OOP_Inheritance
     {
         static void Main(string[] args)
         {
+            Utility.SetUnicodeConsole();
+
+            CourseIT courseIT01 = new CourseIT();
+
+            courseIT01.ReadCourse();
+
+            courseIT01.ListCourse();
+
+
+            Utility.TerminateConsole();
         }
     }
 }
