c4b0d0a0080a604a117769cef08f86ee1ce084af
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-05-28 22:53:46 +0100

    240528

---
+++ E01_OOP_Vehicle_v1/Interfaces/IVehicle.cs
@@ -0,0 +1,27 @@
+﻿using E01_OOP_Vehicle_v1.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01_OOP_Vehicle_v1.Interfaces
+{
+    internal interface IVehicle
+    {
+        #region Properties (public or internal)
+
+        int VehicleId { get; }
+        int NextId { get; }
+        EnumBrand Brand { get; }
+        EnumModel Model { get; }
+        int Year { get; }
+
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
