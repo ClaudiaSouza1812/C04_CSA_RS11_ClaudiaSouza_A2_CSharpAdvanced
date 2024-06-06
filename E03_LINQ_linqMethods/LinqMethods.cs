5d8170a2068e99ed1d654a3a8e610c36203b54a1
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-06-06 22:47:46 +0100

    240606

--- E03_LINQ_linqMethods/LinqMethods.cs
+++ E03_LINQ_linqMethods/LinqMethods.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D00_Utility;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,44 @@ namespace E03_LINQ_linqMethods
 {
     internal class LinqMethods
     {
-        #region Variáveis da classe
+        #region Fields (Variáveis da classe)
 
         internal List<TimeSpan> timepanList = new List<TimeSpan>();
 
         #endregion
 
+        #region CitiesCustomersMethodSyntax
+
+        internal void CitiesCustomersMethodSyntax()
+        {
+            CreateListTimespan();
+
+            Utility.WriteTitle("MethodAndQuerySyntax - Cities and Customers", "", "\n\n");
+            FindAllTimespans();
+
+        }
+
+        #endregion
+
         #region Methods
+
+        #region Listar timespans
+
+        internal void ListTimespans()
+        {
+            CreateListTimespan();
+
+            Utility.WriteTitle("Lista de timespans", "", "\n\n");
+
+            foreach (var item in timepanList)
+            {
+                Utility.WriteMessage(item.ToString(), "\n");
+            }
+        }
+
+        #endregion
+
+        #region 1.1.  Criar uma timespan de números aleatórios entre 0 e 24 horas.
 
         internal void CreateListTimespan()
         {
@@ -36,5 +68,34 @@ namespace E03_LINQ_linqMethods
         }
 
         #endregion
+
+        #region 1.2.  Usar  FindAll()  para  localizar  todas  as  timespans  inferiores  a  12 horas.
+
+        // var timesList = timepanList.FindAll(t => t < new TimeSpan(12, 0, 0));
+
+        internal void FindAllTimespans()
+        {
+            var timesList = timepanList.FindAll(t => t.Hours < 12);
+
+            Utility.WriteTitle("Usar  FindAll()  para  localizar  todas  as  timespans  inferiores  a  12 horas.", "", "\n\n");
+
+            foreach (var item in timesList)
+            {
+                Utility.WriteMessage(item.ToString(), "", "\n");
+            };
+
+            ListTimespans();
+        }
+
+        #endregion
+
+
+        #endregion
+
+
+
+        
+
+        
     }
 }
