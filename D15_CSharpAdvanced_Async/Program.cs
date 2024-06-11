89b13e5f053e12a3b6e887e35b71b8fdc8297116
Claudia Simone de Souza. <claudia.simmone@gmail.com>
2024-06-11 22:26:56 +0100

    240611

--- D15_CSharpAdvanced_Async/Program.cs
+++ D15_CSharpAdvanced_Async/Program.cs
@@ -8,8 +8,35 @@ namespace D15_CSharpAdvanced_Async
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+        static async Task Main(string[] args) // Main thread --> async
         {
+            /*
+            #region Async01
+            // Start the two tasks
+            Task task01 = ClassAsync01.Task01();
+            Task task02 = ClassAsync01.Task02();
+
+            // Wait for the tasks to finish
+            await Task.WhenAll(task01, task02);
+
+            Console.WriteLine("Tarefas terminadas", "", "\n");
+
+            #endregion
+            */
+            #region Async02
+
+            Task task03 = ClassAsync01.Task04("03", 1000);
+            Task task04 = ClassAsync01.Task04("04", 4000);
+            
+            await Task.WhenAll(task03, task04);
+
+            Console.WriteLine("Tarefas terminadas", "", "\n");
+
+            #endregion
+
+
+            Console.ReadKey();
         }
     }
 }
