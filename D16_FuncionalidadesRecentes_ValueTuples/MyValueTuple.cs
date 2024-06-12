9dd8ebac3b153528bf4bed661b037fb8e3ff5bcc
Claudia Simone de Souza. <claudia.simmone@gmail.com>
2024-06-12 20:34:25 +0100

    240612

---
+++ D16_FuncionalidadesRecentes_ValueTuples/MyValueTuple.cs
@@ -0,0 +1,17 @@
+﻿using D00_Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D16_FuncionalidadesRecentes_ValueTuples
+{
+    internal class MyValueTuple
+    {
+        internal static void ListValueTuple((string, int) student)
+        {
+            Utility.WriteMessage($"Nome: {student.Item1}, Idade: {student.Item2}", "", "\n");
+        }
+    }
+}
