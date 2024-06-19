eef109a690d15af8f0d8099300078230cca44849
Claudia Simone de Souza. <claudia.simmone@gmail.com>
2024-06-19 22:27:32 +0100

    240619

--- E03_LINQ_linqMethods/LinqMethods.cs
+++ E03_LINQ_linqMethods/LinqMethods.cs
@@ -12,7 +12,7 @@ namespace E03_LINQ_linqMethods
     {
         #region Fields (Variáveis da classe)
 
-        internal List<TimeSpan> timepanList = new List<TimeSpan>();
+        internal List<TimeSpan> timespanList = new List<TimeSpan>();
 
         #endregion
 
@@ -23,7 +23,16 @@ namespace E03_LINQ_linqMethods
             CreateListTimespan();
 
             Utility.WriteTitle("MethodAndQuerySyntax - Cities and Customers", "", "\n\n");
+
             FindAllTimespans();
+
+            ListTimespans();
+
+            ExistNumber();
+
+            Between0and24();
+
+            ReturnHour();
 
         }
 
@@ -31,21 +40,7 @@ namespace E03_LINQ_linqMethods
 
         #region Methods
 
-        #region Listar timespans
-
-        internal void ListTimespans()
-        {
-            CreateListTimespan();
-
-            Utility.WriteTitle("Lista de timespans", "", "\n\n");
-
-            foreach (var item in timepanList)
-            {
-                Utility.WriteMessage(item.ToString(), "\n");
-            }
-        }
-
-        #endregion
+        
 
         #region 1.1.  Criar uma timespan de números aleatórios entre 0 e 24 horas.
 
@@ -63,7 +58,7 @@ namespace E03_LINQ_linqMethods
             for (int i = 0; i < 10; i++)
             {
                 int minutes = random.Next(maxMinutes);
-                timepanList.Add(start.Add(TimeSpan.FromMinutes(minutes)));
+                timespanList.Add(start.Add(TimeSpan.FromMinutes(minutes)));
             }
         }
 
@@ -75,49 +70,101 @@ namespace E03_LINQ_linqMethods
 
         internal void FindAllTimespans()
         {
-            var timesList = timepanList.FindAll(t => t.Hours < 12);
-
             Utility.WriteTitle("Usar  FindAll()  para  localizar  todas  as  timespans  inferiores  a  12 horas.", "", "\n\n");
+
+            var timesList = timespanList.FindAll(t => t.Hours < 12);
 
             foreach (var item in timesList)
             {
                 Utility.WriteMessage(item.ToString(), "", "\n");
             };
 
-            ListTimespans();
         }
 
         #endregion
 
         #region 1.3.  Usar Exists() para verificar se alguma timespan tiver 5 na propriedade Hours.
 
-        // var timesList = timepanList.FindAll(t => t < new TimeSpan(12, 0, 0));
 
         internal void ExistNumber()
         {
-            
+            Utility.WriteTitle("Usar Exists() para verificar se alguma timespan tiver 5 na propriedade Hours.", "\n", "\n\n");
+
+            var exist = timespanList.Exists(t => t.Hours == 5);
+
+            if (exist)
+            {
+                Utility.WriteMessage("As timespans com '5' na hora são:", "", "\n");
+
+                var timeList = timespanList.FindAll(t => t.Hours == 5);
+
+                foreach (TimeSpan time in timeList)
+                {
+                    Utility.WriteMessage(time.ToString(), "", "\n");
+                }            
+            }
+            else
+            {
+                Utility.WriteMessage("Não existem timespans com '5' na hora.");
+            }
         }
 
         #endregion
 
-        #region 1.4.  Usar TrueForAll() para garantir que todas as timespans estão entre 0 e 24 horas.
+        #region 1.4. Usar TrueForAll() para garantir que todas as timespans estão entre 0 e 24 horas.
 
-        // var timesList = timepanList.FindAll(t => t < new TimeSpan(12, 0, 0));
 
         internal void Between0and24()
         {
-            
+            Utility.WriteTitle("Usar TrueForAll() para garantir que todas as timespans estão entre 0 e 24 horas.", "\n", "\n\n");
+
+            var isAllTrue = timespanList.TrueForAll(t => t.Hours >= 0 && t.Hours <= 24);
+
+            if (!isAllTrue) 
+            {
+                Utility.WriteMessage("As timespans fora do range entre 0 e 24 horas são:", "", "\n");
+
+                var timeList = timespanList.Where(t => t.Hours < 0 || t.Hours > 24).ToList();
+
+                foreach (TimeSpan time in timeList)
+                {
+                    Utility.WriteMessage(time.ToString(), "", "\n");
+                }
+            }
+            else
+            {
+                Utility.WriteMessage("Todas as horas dentro do range entre 0 e 24 horas");
+            }
         }
 
         #endregion
 
-        #region 1.5.  Usar  ConvertAll()  para  retornar  só  a  parte  de  Hours  de  cada timespan.
-
-        // var timesList = timepanList.FindAll(t => t < new TimeSpan(12, 0, 0));
+        #region 1.5. Usar  ConvertAll()  para  retornar  só  a  parte  de  Hours  de  cada timespan.
 
         internal void ReturnHour()
         {
-           
+            Utility.WriteTitle("Usar  ConvertAll()  para  retornar  só  a  parte  de  Hours  de  cada timespan.", "\n", "\n\n");
+
+            var timeList = timespanList.ConvertAll(t => t.Hours);
+
+            foreach (int time in timeList)
+            {
+                Utility.WriteMessage(time.ToString(), "", "\n");
+            }
+        }
+
+        #endregion
+
+        #region Listar timespans
+
+        internal void ListTimespans()
+        {
+            Utility.WriteTitle("Lista de timespans", "", "\n");
+
+            foreach (var item in timespanList)
+            {
+                Utility.WriteMessage(item.ToString(), "", "\n");
+            }
         }
 
         #endregion
--- E04_ExtensionMethod/Program.cs
+++ E04_ExtensionMethod/Program.cs
@@ -20,23 +20,38 @@ namespace E04_ExtensionMethod
 
             Person person01 = new Person("Claudia", "Souza", "31", Person.EnumMaritalStatus.Married, Person.EnumEyeColor.Brown, Person.EnumHairColor.Brown);
 
+            #region Extension Method
+
             // 1. Extension Method - Criar um método de extensão para a classe Person que devolve o nome completo da pessoa.
 
             Utility.WriteTitle("1. Extension method - return the person full name", "", "\n");
-            Utility.WriteMessage(PersonExtensions.ListPersonFullName(person01));
+            Utility.WriteMessage(person01.ListPersonFullName
+                ());
+
+            #endregion
+
+            #region Tuple
 
             // 2. Tuple - Criar um método que devolve um tuple com o nome completo e a idade da pessoa.
 
             Utility.WriteTitle("2. Tuple - return of a tuple with person name and age", "\n", "\n");
-            Utility.WriteMessage($"Nome: {PersonExtensions.ListPersonFullNameAndAge(person01).Item1} {PersonExtensions.ListPersonFullNameAndAge(person01).Item2}, Idade: {PersonExtensions.ListPersonFullNameAndAge(person01).Item3}");
+            Utility.WriteMessage($"Nome: {person01.ListPersonFullNameAndAge().Item1} {person01.ListPersonFullNameAndAge().Item2}, Idade: {person01.ListPersonFullNameAndAge().Item3}");
+
+            #endregion
+
+            #region Discard
 
             // 3. Discard - Usar o discard para desestruturar o tuple devolvido pelo método GetPersonInfo.
-            
+
             Utility.WriteTitle("3. Discard - Destructure a returned tuple", "\n", "\n");
 
             var (name, _) = Person.GetPersonInformation();
 
             Utility.WriteMessage(name);
+
+            #endregion
+
+            #region Deconstruct
 
             // 4. Deconstruct - Implementar o método Deconstruct na classe Person para permitir a desestruturação de objetos Person.
 
@@ -49,7 +64,10 @@ namespace E04_ExtensionMethod
 
             var (_, lastName, _, maritalStatus, eyeColor, _) = person02;
 
-            Utility.WriteMessage($"Last Name: {lastName}, Marital Status: {maritalStatus}, Eye Color: {eyeColor}"); 
+            Utility.WriteMessage($"Last Name: {lastName}, Marital Status: {maritalStatus}, Eye Color: {eyeColor}");
+
+            #endregion
+
 
             Utility.TerminateConsole();
         }
