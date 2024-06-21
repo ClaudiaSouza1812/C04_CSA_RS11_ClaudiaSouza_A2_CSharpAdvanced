f0db5776a79aedd2e6c025b07ed8d189dd957c07
Claudia Simone de Souza. <claudia.simmone@gmail.com>
2024-06-21 17:42:53 +0100

    240621

---
+++ D21_SOLID_05_DIP_V2/Classes/NameFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D21_SOLID_05_DIP_V1
+{
+    internal class NameFormatter : INameFormatter
+    {
+        public string FormatName(Person person)
+        {
+            return $"{person.FirstName} {person.LastName}";
+        }
+
+        public string FormatName(Employee employee)
+        {
+            return $"{employee.FirstName} {employee.LastName}";
+        }
+    }
+}
